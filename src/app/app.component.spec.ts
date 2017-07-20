e0488a6c1f1c1992826643a9f82b57c9ffcd6a12
Alex <Alexander.Dewey@highpointsolutions.com>
2017-07-20 09:59:17 -0400

    Always see questions first from search results

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,10 +17,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'RFP Search'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('RFP Search');
   }));
 
   it('should render title in a h1 tag', async(() => {
